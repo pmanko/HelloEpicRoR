b26017bad2e9fa50b764dae97a140d539dacd61f
Piotr Mankowski <piotr.mankowski@gmail.com>
2017-02-28 00:34:11 -0800

    testing something

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,24 @@ class PagesController < ApplicationController
 
 
   def fhir_index
+    @state = params[:state]
+    @code = params[:code]
+
+
+
+
+
+
+    render :patient
+
     #render layout: false
   end
 
   def fhir_launch
+
+
+
+
     #render layout: false
   end
 
