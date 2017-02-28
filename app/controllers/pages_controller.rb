a342df6a926cbe994cc75a5e09717dccb0366a61
Piotr Mankowski <piotr.mankowski@gmail.com>
2017-02-28 00:47:40 -0800

    testing something

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,5 @@
+require 'net/http'
+
 class PagesController < ApplicationController
 
 
@@ -16,10 +18,30 @@ class PagesController < ApplicationController
   end
 
   def fhir_launch
+    @serviceUri = params["iss"]
+    @launchContextId = params["launch"]
+    @scope = "patient/*.read launch"
 
 
+    @launchUri = "https://enigmatic-brushlands-72564.herokuapp.com/flaunch"
+    @redirectUri = "https://enigmatic-brushlands-72564.herokuapp.com/findex"
+
+    @conformanceUri = "#{@serviceUri}/metadata"
 
 
+    clientId = "4bc7b02f-7a71-45d8-9b98-5181458e79ac"
+    secret = "ALBJ1YiX4Ieto_vrgvPP3s2SM-zO5cwQlXCSXfsZC4ZJkN-Q2w9sh-wmkW1UwSYXI9Ao-NsjAEyNPw-SzfeV6Nc"
+
+
+    request = Net::HTTP::Get.new(@conformanceUri)
+    result = Net::HTTP.start(@conformanceUri) {|http|
+      http.request(request)
+
+    }
+
+    @body = result.body
+
+    render :patient
     #render layout: false
   end
 
