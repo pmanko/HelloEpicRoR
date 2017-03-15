c41e702377cfacb20c9253e9c9a04a829d6754e8
Piotr Mankowski <piotr.mankowski@gmail.com>
2017-03-15 13:36:59 -0500

    MyChart integration

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get 'findex', to: 'pages#fhir_index', as: :fhir_index
   get 'flaunch', to: 'pages#fhir_launch', as: :fhir_launch
   get 'elaunch', to: 'pages#epic_launch', as: :ehir_launch
+  get 'mlaunch', to: 'pages#mychart_launch', as: :mhir_launch
   get 'launch', to: 'pages#local_launch', as: :local_launch
   get 'remote', to: 'pages#remote_launch', as: :remote_launch
 end
